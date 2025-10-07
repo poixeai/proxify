74a126cbe66b893cd6df8c8fb538f7e5e18e3b86
leon <leon@leondeMacBook-Pro.local>
2025-10-07 19:54:37 +0800

    add workflow

---
+++ web/vite.config.ts
@@ -0,0 +1,24 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  // 加载环境变量
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    server: {
+      host: env.VITE_HOST || 'localhost',
+      port: parseInt(env.VITE_PORT) || 5173,
+      open: false, // 自动打开浏览器
+    },
+  }
+})
